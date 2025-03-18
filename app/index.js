53d657f10c22a5b7be45e71ec6ba460c41ed5f99
ginzagom <kds111177@gmail.com>
2025-03-18 19:24:28 +0900

    스크린 수정

--- app/index.js
+++ app/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity, TextInput, Image } from 'react-native';
-import { textSpanContainsPosition } from 'typescript';
+import { View, Text, SafeAreaView, TouchableOpacity, TextInput, Image, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
+     
+      <View style={styles.topRightImageContainer}>
+        <Image source={require('../assets/images/usericon.png')} style={styles.topRightImage} />
+      </View>
+
       <View style={styles.content}>
         <Text style={styles.title}>
           <Text style={styles.easyText}>Easy</Text>
           <Text style={styles.packText}>Pack</Text>
         </Text>
 
-        <TextInput style={styles.searchBox}  />
+        {/* 🔹 검색 박스 안에 search.png 아이콘 추가 */}
+        <View style={styles.searchContainer}>
+          <TextInput style={styles.searchBox} placeholder="검색어 입력" />
+          <Image source={require('../assets/images/search.png')} style={styles.searchIcon} />
+        </View>
 
         <View style={styles.menuContainer}>
           <View style={styles.menuItem}>
@@ -29,9 +37,14 @@ const HomeScreen = () => {
         </View>
 
         <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button1}>
-            <Text style={styles.buttonText1}>나의 항공사 규정 확인하러 가기</Text>
+         
+          <TouchableOpacity style={styles.button1}>
+            <View style={styles.buttonContent}>
+              <Text style={styles.buttonText1}>나의 항공사 규정 확인하러 가기</Text>
+              <Image source={require('../assets/images/point.png')} style={styles.buttonImage} />
+            </View>
           </TouchableOpacity>
+          
           <TouchableOpacity style={styles.button2}>
             <Text style={styles.buttonText2}>여행지 사진</Text>
           </TouchableOpacity>
@@ -44,7 +57,7 @@ const HomeScreen = () => {
   );
 };
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -72,16 +85,36 @@ const styles = {
     fontSize: 30,
     fontWeight: 'bold',
   },
-  searchBox: {
+
+ 
+  searchContainer: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
     width: '80%',
-    height: 40,
     borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 8,
     paddingHorizontal: 10,
-    textAlign: 'center',
     marginBottom: 20,
+    position: 'relative', 
   },
+
+
+  searchBox: {
+  
+    flex: 1, 
+    height: 40,
+    paddingLeft: 10,
+  },
+
+  
+  searchIcon: {
+    width: 20,
+    height: 20,
+    position: 'absolute',
+    right: 10, 
+  },
+
   menuContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
@@ -92,21 +125,9 @@ const styles = {
     alignItems: 'center',
     marginHorizontal: 20,
   },
-  icon1: {
-    width: 50,
-    height: 50,
-    marginBottom: 5,
-  },
-  icon2: {
-    width: 50,
-    height: 50,
-    marginBottom: 5,
-  },
-  icon3: {
-    width: 50,
-    height: 50,
-    marginBottom: 5,
-  },
+  icon1: { width: 50, height: 50, marginBottom: 5 },
+  icon2: { width: 50, height: 50, marginBottom: 5 },
+  icon3: { width: 50, height: 50, marginBottom: 5 },
   menuText: {
     fontSize: 18,
     color: 'black',
@@ -116,11 +137,11 @@ const styles = {
     alignItems: 'center',
     width: '100%',
   },
+
   button1: {
     backgroundColor: '#ffffff',
     padding: 15,
     borderRadius: 15,
-    alignItems: 'center',
     width: '80%',
     height: 50,
     marginBottom: 20,
@@ -128,42 +149,68 @@ const styles = {
   button2: {
     backgroundColor: '#c8d7eb',
     padding: 15,
-    borderRadius: 7,
-    alignItems: 'center',
-    width: '80%',
-    height: 280,
-    marginBottom: 10,
+    borderRadius: 15,
+    width: '85%',
+    height: 250,
+    marginBottom: 20,
   },
   button3: {
     backgroundColor: '#c8d7eb',
     padding: 15,
-    borderRadius: 7,
-    alignItems: 'center',
-    width: '80%',
+    borderRadius: 15,
+    width: '85%',
     height: 100,
-    marginBottom: 10,
+    marginBottom: 20,
   },
+
+  buttonContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems: 'center',
+    width: '100%',
+  },
+
   buttonText1: {
     color: 'grey',
     fontSize: 16,
     fontWeight: 'bold',
-    textAlign: 'center',
-   
+    textAlign: 'left',
+    marginLeft: 10, 
   },
+  
+
+  
+  buttonImage: {
+    width: 20,
+    height: 20,
+    marginRight: 10, 
+  },
+
   buttonText2: {
-    color: 'white',
+    color: 'black',
     fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'center',
-   
   },
   buttonText3: {
-    color: 'white',
+    color: 'black',
     fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'center',
-   
   },
-};
+
+  
+  topRightImageContainer: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+
+  topRightImage: {
+    width: 40,
+    height: 40,
+  },
+});
 
 export default HomeScreen;
+
